2668c9c2823f67f27f597e977e6c60c2e32ef4b8
satyam mishra <satyam.mishra975@gmail.com>
2021-08-28 15:19:47 +0530

    updating readme

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,17 @@
+import WindiCSS from 'vite-plugin-windicss';
+import node from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter:node(),
+		vite: {
+			plugins: [
+				WindiCSS.default()
+			]
+		}
 	}
 };
 
