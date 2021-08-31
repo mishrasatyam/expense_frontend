4646510b26abde654df5b54c0a39ca3c539c9953
satyam mishra <satyam.mishra975@gmail.com>
2021-08-31 12:27:52 +0530

    add prod api utl

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,2 +1,3 @@
-export const api_url = `http://localhost:8000`
+import {dev} from '$app/env'
+export const api_url = dev?`http://localhost:8000`:'https://expenseapi.satyam.life'
 export const headers = {'content-type':'application/json'}
