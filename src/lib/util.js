bb1c0b483ddbf1283a0a541ce3746d19d008a13d
satyam mishra <satyam.mishra975@gmail.com>
2021-09-06 07:34:47 +0530

    test

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,3 +1,3 @@
 import {dev} from '$app/env'
-export const api_url = dev?`http://localhost:8000`:'https://expenseapi.satyam.life'
+export const api_url = dev?`http://localhost:8000`:'https://api.expense.satyam.life/'
 export const headers = {'content-type':'application/json'}
